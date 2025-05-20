9036ac3669785feef11c3db942637fb24a83a636
Azime Tolumoğlu <azimet@bilge.com.tr>
2025-05-20 14:31:46 +0300

    sıralama metodu eklendi.

--- Repository/Extensions/BookRepositoryExtensions.cs
+++ Repository/Extensions/BookRepositoryExtensions.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Entities.Models;
+using System.Linq.Dynamic.Core;
+using Repository.Extensions;
 
 namespace Repository.EFCore
 {
@@ -26,5 +29,46 @@ namespace Repository.EFCore
                 .ToLower()
                 .Contains(searchTerm));
         }
-     }
+
+
+        public static IQueryable<Book> Sort(this IQueryable<Book> books,
+            string orderByQueryString)
+        {
+            if (!string.IsNullOrWhiteSpace(orderByQueryString))
+            {
+                return books.OrderBy(b=> b.Id);
+            }
+            //var orderParams = orderByQueryString.Split(','); //books?orderBy=title,price
+
+            //var propertyInfos = typeof(Book).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            //var orderQueryBuilder = new StringBuilder();
+
+            //title ascending, price descending, id ascending[,]
+            //foreach (var param in orderParams) 
+            //{
+            //    if(string.IsNullOrWhiteSpace(param))
+            //        continue;
+            //    var propertyFromQueryName = param.Split(' ')[0];//price desc gibi olunca 
+            //    var objectProperty = propertyInfos
+            //        .FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName,
+            //        StringComparison.InvariantCultureIgnoreCase));
+            //    if (objectProperty is null)
+            //        continue;
+            //    var direction = param.EndsWith("des") ? "descending" : "ascending";
+            //    orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction},");
+
+            //}
+            //var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+
+            var orderQuery = OrderQueryBuilder.CreateOrderQuery<Book> (orderByQueryString);
+
+            if(orderQuery is null)
+                return books.OrderBy(b => b.Id);
+                return books.OrderBy(b => b.Id);
+
+            return books.OrderBy(orderQuery);
+
+        }
+    }
 }
--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a456f0d167d6459f337a9d9647fa5ea9c119bf5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72a58d06afa85c834aa0061bdb872562738509c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
